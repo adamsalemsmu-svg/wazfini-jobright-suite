d33e7ae0f910061dcfdcae9a20c6bf2227a09cbe
Adam Salem <adamsalemsmu@gmail.com>
2025-10-25 02:34:47 +0000

    Fix: Revert vite config to previous state

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,12 @@ import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: __dirname,
   plugins: [react()],
   build: {
-    outDir: 'dist',
+    outDir: resolve(__dirname, 'dist'),
+    rollupOptions: {
+      input: resolve(__dirname, 'index.html'),
+    },
   },
 })
