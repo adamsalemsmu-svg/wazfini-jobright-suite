195d78c4804727052af0356af7af8be1961bece6
Adam Salem <adamsalemsmu@gmail.com>
2025-10-25 02:38:10 +0000

    Fix: configure Vercel for monorepo build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: __dirname,
   plugins: [react()],
-  build: {
-    outDir: resolve(__dirname, 'dist'),
-    rollupOptions: {
-      input: resolve(__dirname, 'index.html'),
-    },
-  },
 })
