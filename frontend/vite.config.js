1b5aaa95f4155287d725f925a888ce950e614784
Adam Salem <adamsalemsmu@gmail.com>
2025-10-25 02:21:34 +0000

    fix: explicitly set root and build paths in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: __dirname,
   plugins: [react()],
   build: {
     modulePreload: false,
+    outDir: resolve(__dirname, 'dist'),
+    rollupOptions: {
+      input: resolve(__dirname, 'index.html'),
+    },
   },
 })
